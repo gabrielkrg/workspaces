0d5d62aea6e352de9f5e14be630e3abf3d2e1e1e
Gabriel Rodrigues <gabrielkrodrigues1@gmail.com>
2025-04-23 18:15:57 -0300

    minor

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    // return Inertia::render('Welcome');
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
