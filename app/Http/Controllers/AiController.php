e268c47de3db479ccec36d418a92add6f97b078b
Gabriel <gabrielkrodrigues1@gmail.com>
2025-10-24 17:49:55 +0800

    change token name

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -15,7 +15,7 @@ class AiController extends Controller
         ]);
 
         $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . env('AI_API_TOKEN'),
+            'Authorization' => 'Bearer ' . env('API_AI_TOKEN'),
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
         ])->post('https://ai.gabrielk.dev/api/ai/019970a8-679d-70bb-a6c9-29cdc4d2d4ea', [
