e6237e3f84dbc31a05d38401ca4687e786dade3a
Gabriel <gabrielkrodrigues1@gmail.com>
2025-10-24 17:51:41 +0800

    change endpoint

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -18,7 +18,7 @@ class AiController extends Controller
             'Authorization' => 'Bearer ' . env('API_AI_TOKEN'),
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
-        ])->post('https://ai.gabrielk.dev/api/ai/019970a8-679d-70bb-a6c9-29cdc4d2d4ea', [
+        ])->post('https://ai.gabrielk.dev/api/ai/019a159a-b497-7098-9353-4dde59275045', [
             'content' => $validated['content'],
         ]);
 
